c1e23556bb660bffa570bcacef1609084fb183ba
huseynaktas <h.akts1007@gmail.com>
2024-05-06 16:47:51 +0300

    Mini Emoji Rating App

---
+++ emoji-rating/index.js
@@ -0,0 +1,25 @@
+const starsEl = document.querySelectorAll(".fa-star");
+const emojiEl = document.querySelectorAll(".far")
+const colorsArray = ["red","orange","lightblue","lightgreen","green"]
+
+updateRating(0);
+
+starsEl.forEach((starsEl, index) => {
+    starsEl.addEventListener("click", ()=>{
+        updateRating(index)
+    })
+})
+
+function updateRating(index){
+    starsEl.forEach((starsEl, idx)=>{
+        if(idx < index + 1){
+            starsEl.classList.add("active");
+        }else{
+            starsEl.classList.remove("active");
+        }
+    });
+    emojiEl.forEach(emojiEl=>{
+        emojiEl.style.transform = `translateX(-${index * 50}px)`;
+        emojiEl.style.color = colorsArray[index]
+    })
+}
